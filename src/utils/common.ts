0f3b72dde2559697379fd5483138f779c909a5ae
Xiang Guo <xiang.guo@dolphindb.com>
2023-08-15 15:17:59 +0800

    feat: 新增工具类 Observer，订阅发布模式

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -5,3 +5,34 @@ export function getFileFormat(url: string){
     }
     return url
 }
+interface eventList{
+    [key: string]: Array<Function>
+}
+export class Observer {
+    list: eventList
+    constructor () {
+        this.list = {}
+    }
+    
+    on(event: string, fn: Function) {      
+        this.list[event] = this.list[event] || new Array<Function>
+        this.list[event].push(fn)
+        return this.list[event].length - 1
+    }
+    
+    emit(event: string, params: object){
+        if (!this.list[event])
+            throw 'There is no such event'
+        this.list[event].forEach( fn => fn(params) )
+    }
+    
+    off(event: string, eventFn?: Function | null, eventFnId?: number){
+        if (!this.list[event])
+            return false
+        
+        let index = this.list[event].indexOf(this.list[event].find( fn => fn === eventFn ) || function(){} ) || eventFnId
+        
+        if (index)
+            this.list[event].splice(index, 1)
+    }
+}
